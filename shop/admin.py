88e2cd12f0382d48138586a7e93c7aa324b705fb
Chris West <chris@minrivertea.com>
2011-08-09 16:13:18 +0800

    reviews

--- shop/admin.py
+++ shop/admin.py
@@ -7,6 +7,7 @@ class ProductAdmin(admin.ModelAdmin):
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Review)
+admin.site.register(ShopSettings)
 admin.site.register(UniqueProduct)
 admin.site.register(Page)
 
