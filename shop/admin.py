284d00d9a862c2a6c06c376765933eb12cbc9d1c
Chris West <chris@minrivertea.com>
2011-08-25 11:47:28 +0800

    changes

--- shop/admin.py
+++ shop/admin.py
@@ -3,14 +3,17 @@ from django.contrib import admin
 
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
-    
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('invoice_id', 'is_paid', 'owner') 
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Review)
 admin.site.register(ShopSettings)
 admin.site.register(UniqueProduct)
 admin.site.register(Page)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Discount)
+admin.site.register(Address)
 
 
