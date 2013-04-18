90f837d394d871f6ece23ca7245de2be754aad5e
minrivertea <chris@minrivertea.com>
2013-04-18 21:32:30 +0800

    changes to paypal/countries

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -21,10 +21,6 @@ def common(request):
     except:    
         country = 'US'
     
-    if country == '-':
-        country = 'UK'
-    print "COUNTRY IS %s" % country
-    
     # change paypal account depending on location
     if country in EUROPE_INCLUDING_UK:
         context['paypal_receiver_email'] = settings.PAYPAL_RECEIVER_EMAIL
